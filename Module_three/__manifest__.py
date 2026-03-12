bdacbebff49517c861d39feb72d6edac365959cf
seif-moawad11 <semm@odootec.com>
2026-03-12 14:04:58 +0200

    style

--- Module_three/__manifest__.py
+++ Module_three/__manifest__.py
@@ -3,12 +3,17 @@
     'author' : 'xyz',
     'version' : '1.0',
     'category' : '',
-    'depends' : ['base', 'point_of_sale'],
+    'depends' : ['base', 'point_of_sale','sale_management'],
     'data': [
         'security/ir.model.access.csv',
         'views/tb_freelancer_view.xml',
         'views/tb_project_view.xml',
         'views/base_menu.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+        'Module_three/static/src/css/style.css',
+        ],
+    },
     'application' : True,
 }
